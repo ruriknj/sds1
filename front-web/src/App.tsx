da68640169367cc52407ecc226f09156111a1324
ruriknj <ruriknj@gmail.com>
2020-09-16 19:38:41 -0300

    Criando pagina Home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Index';
+import Home from './pages/Home/Index';
+
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home />
     </div>
   );
 }
