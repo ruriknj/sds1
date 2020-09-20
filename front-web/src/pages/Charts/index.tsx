a930904fc024e98da12308d2c05848e110784866
ruriknj <ruriknj@gmail.com>
2020-09-20 14:20:09 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -20,7 +20,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-rurik.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
